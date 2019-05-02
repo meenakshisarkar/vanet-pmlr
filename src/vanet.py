3a0a03025c5456d1d0bf005f5b0ee6124c7ff4bc
meenakshi sarkar <meenakshisarkar7@gmail.com>
2019-05-02 20:30:48 +0530

    Update vanet.py

--- src/vanet.py
+++ src/vanet.py
@@ -7,11 +7,18 @@ from ops import *
 from utils import *
 
 
-class VCPROM(object):
-    def __init__(self, image_size=128, batch_size= 32, c_dim=3, timesteps=10, predict=10, checkpoint_dir=None, training=True):
+class VANET(object):
+    def __init__(self, image_size=128, batch_size= 32, c_dim=3, timesteps=10, predict=10,
+                 checkpoint_dir=None, training=True):
         self.image_size= image_size
         self.batch_size= batch_size
         self.c_dim= c_dim
         self.timesteps= timesteps
         self.predict= predict
-        self.checkpoint_dir=
+        self.checkpoint_dir= checkpoint_dir
+        self.training= training
+        
+        self.vel_shape= [batch_size, timesteps-1, image_size[0], image_size[1], c_dim]
+        self.acc_shape= [batch_size, timesteps-2, image_size[0], image_size[1], c_dim]
+        self.xt_shape= [batch_size, timesteps, image_size[0], image_size[1], c_dim]
+        self.predict_shape=[batch_size, predict, image_size[0], image_size[1], c_dim]
