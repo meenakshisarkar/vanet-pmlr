d69c87cdc5d926e0adc8cf81dbd4afeaa5b63fa0
meenakshi sarkar <meenakshisarkar7@gmail.com>
2019-05-09 02:57:20 +0530

    Update vanet.py

--- src/vanet.py
+++ src/vanet.py
@@ -26,22 +26,37 @@ class VANET(object):
         self.create_model()
     
     
-    def create_model(self):
-        self.velocity= tf.placeholder(tf.float32, self.vel_shape, name= 'velocity')
-        self.accelaration= tf.placeholder(tf.float32, self.acc_shape, name= 'accelaration')
-        self.xt= tf.placeholder(tf.float32, self.xt_shape, name= 'xt')
-        self.predict= tf.placeholder(tf.float32, self.predict_shape, name= 'predict')
-        cell= BasicConvLSTMCell([self.image_size[0]/8, self.image_size[1]/8],
-                             [3, 3], 256)
-        predict= forward_model(self.velocity, self.accelaration, self.xt, cell)
-        self.G = tf.concat(axis=3,values=predict)
+        def create_model(self):
+        self.velocity = tf.placeholder(tf.float32, self.vel_shape, name='velocity')
+        self.accelaration = tf.placeholder(tf.float32, self.acc_shape, name='accelaration')
+        self.xt = tf.placeholder(tf.float32, self.xt_shape, name='xt')
+        self.predict = tf.placeholder(tf.float32, self.predict_shape, name='predict')
+        cell = BasicConvLSTMCell([self.image_size[0] / 8, self.image_size[1] / 8],
+                                 [3, 3], 256)
+        predict = forward_model(self.velocity, self.accelaration, self.xt, cell)
+        self.G = tf.concat(axis=3, values=predict)
 
-    def forward_model(self):
-        state_vel= tf.zeros([self.batch_size, self.image_size[0]/8, self.image_size[1]/8, 512])
-        reuse=False
+    def forward_model(self, diff_in, xt, cell):
+        state_vel = tf.zeros([self.batch_size, self.image_size[0] / 8, self.image_size[1] / 8, 512])
+        reuse_vel = False
+        # Encoder
+        for t in xrange(self.timesteps-1):
+            enc_h, res_m = self.motion_enc(diff_in[:, :, :, t, :], reuse=reuse_vel)
+            h_dyn, state = cell(enc_h, state, scope='lstm', reuse=reuse_vel)
+            reuse_vel = True
+        for t in xrange(self.timesteps-2):
+            enc_h, res_m = self.motion_enc(diff_in[:, :, :, t, :], reuse=reuse_vel)
+            h_dyn, state = cell(enc_h, state, scope='lstm', reuse=reuse_vel)
+            reuse_vel = True
 
-    
+        pred = []
+
     def vel_enc(self):
+        cell1 = BasicConvLSTMCell([self.image_size[0] / 8, self.image_size[1] / 8],
+                                 [3, 3], 256)
+        cell2 = BasicConvLSTMCell([self.image_size[0] / 8, self.image_size[1] / 8],
+                                 [3, 3], 256)
+
         
 
     def acc_end(self):
